c4090418f6f033bd3e66742ec48b3712fee14a14
PearlThoughts-DevOps-Internship/strapi/tree/leticia <leticia888444@gmail.com>
2024-06-25 13:01:01 +0400

    added image

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo apt update && sudo apt install docker.io docker-compose -y
-sudo systemctl enable docker && sudo usermod -aG docker ubuntu
+sudo systemctl enable docker && sudo usermod -aG docker $USER
 git clone https://github.com/leticia2983/Strapi_ec2_docker.git
+sudo chown -R ubuntu:ubuntu /home/ubuntu/Strapi_ec2_docker
+sudo chmod -R 755 /home/ubuntu/Strapi_ec2_docker
 cd Strapi_ec2_docker
 sudo docker run -d -p 1337:1337 leticia888444/leticia_strapi:1.0.
 sleep 2000
